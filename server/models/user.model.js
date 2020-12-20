89ae430ab7abd7649638cf6a2fe1224cc8ad5bf8
EKaxada <encoksdha@gmail.com>
2020-12-20 10:04:57 +0300

    added authentication & encryption of password

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -30,11 +30,31 @@ userSchema
   .virtual("password")
   .set(function (password) {
     this._password = password;
-    this.salt = this.makesalt();
-    this.hashed_password = this.encryptedPassword(password);
+    this.salt = this.makeSalt();
+    this.hashed_password = this.encryptPassword(password);
   })
   .get(function () {
     return this._password;
   });
+
+userSchema.methods = {
+  authenticate: function (PlainText) {
+    return this.encryptPassword(PlainText) === this.hashed_password;
+  },
+  encryptPassword: function (password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  },
+  makeSalt: function () {
+    return Math.round(new Date().valueOf() * Math.random()) + "";
+  },
+};
 
 export default mongoose.model("User", userSchema);
