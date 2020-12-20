70481b5fe8dbcb8e3a1d78f4bfee71bb1e813b01
EKaxada <encoksdha@gmail.com>
2020-12-20 09:39:14 +0300

    served template at root URL in express

--- server/express.js
+++ server/express.js
@@ -3,17 +3,22 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compress from "compression";
 import cors from "cors";
- import helmet from "helmet";
+import helmet from "helmet";
+import Template from "./../template";
 
 const app = express();
 
 /**.....configure express...... */
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(compress());
 app.use(helmet());
 app.use(cors());
+
+app.get("/", (req, res) => {
+  res.status(200).send(Template());
+});
 
 /**....export configuration..... */
 export default app;
