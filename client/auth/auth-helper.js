be4c6254b8942960de7376f7082dc6a27a16cb05
EKaxada <encoksdha@gmail.com>
2020-12-21 08:45:25 +0300

    added auth state methods to track sessions

---
+++ client/auth/auth-helper.js
@@ -0,0 +1,26 @@
+//saving credentials
+authenticate(jwt, cb) {
+    if(typeof window !== "undefined")
+    sessionStorage.setItem('jwt', JSON.stringify(jwt))
+    cb()
+    }
+
+//retrieving credentials
+isAuthenticated() {
+    if (typeof window === "undefined")
+    return false
+
+    if (sessionStorage.getItem("jwt"))
+    return JSON.parse(sessionStorage.getItem("jwt"))
+    return false
+}
+
+//deleting credentials when user signs out
+clearJWT(cb) {
+    if(typeof window !== "undefined")
+    sessionStorage.removeItem("jwt")
+    cb()
+    signout().then((data) =>{
+        document.cookie = "t=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
+    })
+}
