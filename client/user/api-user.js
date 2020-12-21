fe4ceb53f44584c23de6d5a13533200fc64da32d
EKaxada <encoksdha@gmail.com>
2020-12-21 05:10:41 +0300

    added Read function in CRUD

--- client/user/api-user.js
+++ client/user/api-user.js
@@ -1,12 +1,24 @@
 const create = async (user) => {
   try {
-    let response = await fetch("/api/users", {
+    let response = await fetch("/api/users/", {
       method: "POST",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
       body: JSON.stringify(user),
+    });
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const list = async (signal) => {
+  try {
+    let response = await fetch("/api/users/", {
+      method: "GET",
+      signal: signal,
     });
     return await response.json();
   } catch (error) {
