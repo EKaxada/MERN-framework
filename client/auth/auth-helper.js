489a5882634ba54f99f8cca2d0027fa728d07d25
EKaxada <encoksdha@gmail.com>
2020-12-22 09:41:21 +0300

    added missing object export for methods

--- client/auth/auth-helper.js
+++ client/auth/auth-helper.js
@@ -1,27 +1,30 @@
-//saving credentials
-authenticate(jwt, cb) {
-    if(typeof window !== "undefined")
-    sessionStorage.setItem('jwt', JSON.stringify(jwt))
-    cb()
-    }
+import { signout } from "./api-auth.js";
 
-//retrieving credentials
-isAuthenticated() {
-    if (typeof window === "undefined")
-    return false
+//saving credentials
+const auth = {
+  authenticate(jwt, cb) {
+    if (typeof window !== "undefined")
+      sessionStorage.setItem("jwt", JSON.stringify(jwt));
+    cb();
+  },
+
+  //retrieving credentials
+  isAuthenticated() {
+    if (typeof window === "undefined") return false;
 
     if (sessionStorage.getItem("jwt"))
-    return JSON.parse(sessionStorage.getItem("jwt"))
-    return false
-}
+      return JSON.parse(sessionStorage.getItem("jwt"));
+    return false;
+  },
 
-//deleting credentials when user signs out
-clearJWT(cb) {
-    if(typeof window !== "undefined")
-    sessionStorage.removeItem("jwt")
-    cb()
-    signout().then((data) =>{
-        document.cookie = "t=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
-    })
-}
+  //deleting credentials when user signs out
+  clearJWT(cb) {
+    if (typeof window !== "undefined") sessionStorage.removeItem("jwt");
+    cb();
+    signout().then((data) => {
+      document.cookie = "t=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    });
+  },
+};
 
+export default auth;
