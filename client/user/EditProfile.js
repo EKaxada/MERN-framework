e36705cba92f1f13367cd05644e49415aab6e5dc
EKaxada <encoksdha@gmail.com>
2020-12-22 10:13:08 +0300

    overahauled component

--- client/user/EditProfile.js
+++ client/user/EditProfile.js
@@ -1,35 +1,50 @@
 import {
   Button,
   Card,
+  CardActions,
   CardContent,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
+  Icon,
   TextField,
   Typography,
 } from "@material-ui/core";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import auth from "./../auth/auth-helper";
+import { read, update } from "./api-user.js";
 import { makeStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   card: {
     maxWidth: 600,
     margin: "auto",
+    textAlign: "center",
     marginTop: theme.spacing(5),
+    paddingBottom: theme.spacing(2),
   },
   title: {
     padding: `${theme.spacing(3)}px ${theme.spacing(2.5)}px
           ${theme.spacing(2)}px`,
     color: theme.palette.openTitle,
   },
+  error: {
+    verticalAlign: "middle",
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 300,
+  },
   media: {
     minHeight: 400,
   },
+  submit: {
+    margin: "auto",
+    marginBottom: theme.spacing(2),
+  },
 }));
 
-export default function EditProfile() {
+export default function EditProfile({ match }) {
   const classes = useStyles();
   const [values, setValues] = useState({
     name: "",
@@ -37,7 +52,30 @@ export default function EditProfile() {
     email: "",
     open: false,
     error: "",
+    redirectToProfile: false,
   });
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    const signal = abortController.signal;
+
+    read(
+      {
+        userId: match.params.userId,
+      },
+      { t: jwt.token },
+      signal
+    ).then((data) => {
+      if (data && data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setValues({ ...values, name: data.name, email: data.email });
+      }
+    });
+    return function cleanup() {
+      abortController.abort();
+    };
+  }, [match.params.userId]);
 
   const handleChange = (name) => (event) => {
     setValues({ ...values, [name]: event.target.value });
@@ -72,67 +110,65 @@ export default function EditProfile() {
   };
 
   return (
-    <div>
-      <Card className={classes.card}>
-        <CardContent>
-          <Typography variant="h6" className={classes.title}>
-            Sign Up
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography variant="h6" className={classes.title}>
+          Sign Up
+        </Typography>
+        <TextField
+          id="name"
+          label="Name"
+          className={classes.TextField}
+          values={values.name}
+          onchange={handleChange("name")}
+          margin="normal"
+        />
+
+        <br />
+
+        <TextField
+          id="email"
+          type="email"
+          label="Email"
+          className={classes.TextField}
+          value={values.email}
+          onchange={handleChange("email")}
+          margin="normal"
+        />
+
+        <br />
+
+        <TextField
+          id="password"
+          type="password"
+          label="Password"
+          className={classes.textField}
+          value={values.password}
+          onChange={handleChange("password")}
+          margin="normal"
+        />
+
+        <br />
+
+        {values.error && (
+          <Typography component="p" color="error">
+            <Icon color="error" className={classes.error}>
+              error
+            </Icon>
+            {values.error}
           </Typography>
-          <TextField
-            id="name"
-            label="Name"
-            className={classes.TextField}
-            values={values.name}
-            onchange={handleChange("name")}
-            margin="normal"
-          />
-
-          <br />
-
-          <TextField
-            id="email"
-            type="email"
-            label="Email"
-            className={classes.TextField}
-            value={values.email}
-            onchange={handleChange("email")}
-            margin="normal"
-          />
-
-          <br />
-
-          {values.error && (
-            <Typography component="p" color="error">
-              <Icon color="error" className={classes.error}>
-                error
-              </Icon>
-              {values.error}
-            </Typography>
-          )}
-        </CardContent>
-        <CardActions>
-          <Button
-            color="primary"
-            variant="contained"
-            onClick={clickSubmit}
-            className={classes.submit}
-          >
-            Submit
-          </Button>
-        </CardActions>
-      </Card>
-
-      <Dialog open={values.open} disableBackdropClick={true}>
-        <DialogTitle>New Account</DialogTitle>
-        <DialogContentText>New account successfully created</DialogContentText>
-        <DialogActions>
-          <Link to="/signin">
-            <Button color="primary" autoFocus="autoFocus" variant="contained">
-              Sign In
-            </Button>
-          </Link>
-        </DialogActions>
-      </Dialog>
-    </div>
+        )}
+      </CardContent>
+      <CardActions>
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={clickSubmit}
+          className={classes.submit}
+        >
+          Submit
+        </Button>
+      </CardActions>
+    </Card>
   );
 }
