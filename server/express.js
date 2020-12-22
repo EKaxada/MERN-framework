b92d6ca1d22b8e97e044c3b156f567f826e56f5d
EKaxada <encoksdha@gmail.com>
2020-12-22 11:05:11 +0300

    organised file

--- server/express.js
+++ server/express.js
@@ -7,7 +7,6 @@ import helmet from "helmet";
 import Template from "./../template";
 import userRoutes from "./routes/user.routes";
 import authRoutes from "./routes/auth.routes";
-import devBundle from "./devBundle";
 import path from "path";
 
 //modules to render react components to string
@@ -22,21 +21,33 @@ import MainRouter from "./../client/MainRouter";
 import { ServerStyleSheets, ThemeProvider } from "@material-ui/styles";
 import theme from "./../client/theme";
 
+//comment out before building for production
+import devBundle from "./devBundle";
+
 const CURRENT_WORKING_DIR = process.cwd();
 const app = express();
 
+//comment out before building for production
 devBundle.compile(app);
 
-/**.....configure express...... */
+// parse body params and attache them to req.body
 app.use("/dist", express.static(path.join(CURRENT_WORKING_DIR, "dist")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(compress());
+
+// secure apps by setting various HTTP headers
 app.use(helmet());
+
+// enable CORS - Cross Origin Resource Sharing
 app.use(cors());
+
+//mounting routes
 app.use("/", userRoutes);
 app.use("/", authRoutes);
+
+//catch unathousrized errors
 app.use((err, req, res, next) => {
   if (err.name === "UnauthorizedError") {
     res.status(401).json({ error: err.name + ": " + err.message });
