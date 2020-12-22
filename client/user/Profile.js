18ad8b85c1335d6fb2e092623d8f927af2e1857d
EKaxada <encoksdha@gmail.com>
2020-12-22 10:20:24 +0300

    rebuilt component

--- client/user/Profile.js
+++ client/user/Profile.js
@@ -1,6 +1,7 @@
 import {
   Avatar,
   Divider,
+  IconButton,
   List,
   ListItem,
   ListItemAvatar,
@@ -10,22 +11,23 @@ import {
   Typography,
 } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
+import { Edit, Person } from "@material-ui/icons";
 import { makeStyles } from "@material-ui/core/styles";
-import { Link } from "react-router-dom";
+import DeleteUser from "./DeleteUser";
+import auth from "./../auth/auth-helper";
+import { read } from "./api-user.js";
+import { Link, Redirect } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
-  card: {
+  root: theme.mixins.gutters({
     maxWidth: 600,
     margin: "auto",
+    padding: theme.spacing(3),
     marginTop: theme.spacing(5),
-  },
+  }),
   title: {
-    padding: `${theme.spacing(3)}px ${theme.spacing(2.5)}px
-      ${theme.spacing(2)}px`,
-    color: theme.palette.openTitle,
-  },
-  media: {
-    minHeight: 400,
+    marginTop: theme.spacing(3),
+    color: theme.palette.protectedTitle,
   },
 }));
 
@@ -33,6 +35,8 @@ export default function Profile({ match }) {
   const classes = useStyles();
   const [user, setUser] = useState({});
   const [redirectToSignin, setRedirectToSignin] = useState(false);
+
+  const jwt = auth.isAuthenticated();
 
   useEffect(() => {
     const abortController = new AbortController();
@@ -58,42 +62,41 @@ export default function Profile({ match }) {
   }, [match.params.userId]);
 
   if (redirectToSignin) {
-    return <redirect to="/signin" />;
+    return <Redirect to="/signin" />;
   }
 
   return (
-    <div>
-      <Paper className={classes.root} elevation={4}>
-        <Typography>
-          <List dense>
-            <ListItem>
-              <ListItemAvatar>
-                <Avatar>
-                  <Person />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary={user.name} secondary={user.email} />
-            </ListItem>
-            <Divider />
-            <ListItem>
-              <ListItemText
-                primary={"Joined: " + new Date(user.created).toDateString()}
-              />
-            </ListItem>
-          </List>
-        </Typography>
-      </Paper>
-      {auth.isAunthenticated().user &&
-        auth.isAunthenticated().user._id == user._id && (
-          <ListItemSecondaryAction>
-            <Link to={"/user/edit/" + user._id}>
-              <IconButton aria-label="Edit" color="primary">
-                <Edit />
-              </IconButton>
-            </Link>
-            <DeleteUser userId={user._id} />
-          </ListItemSecondaryAction>
-        )}
-    </div>
+    <Paper className={classes.root} elevation={4}>
+      <Typography variant="h6" className={classes.title}>
+        Profile
+      </Typography>
+      <List dense>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              <Person />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary={user.name} secondary={user.email} />{" "}
+          {auth.isAuthenticated().user &&
+            auth.isAuthenticated().user._id == user._id && (
+              <ListItemSecondaryAction>
+                <Link to={"/user/edit/" + user._id}>
+                  <IconButton aria-label="Edit" color="primary">
+                    <Edit />
+                  </IconButton>
+                </Link>
+                <DeleteUser userId={user._id} />
+              </ListItemSecondaryAction>
+            )}
+        </ListItem>
+        <Divider />
+        <ListItem>
+          <ListItemText
+            primary={"Joined: " + new Date(user.created).toDateString()}
+          />
+        </ListItem>
+      </List>
+    </Paper>
   );
 }
